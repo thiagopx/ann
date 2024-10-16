278343dfff91c54ade6446c2a32e0bb932cdb5ac
thiagopx <paixao@gmail.com>
2024-10-15 21:40:44 -0300

    update examples

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,6 +3,48 @@ from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 
 
+def min_max_scaler(X, min_val=None, max_val=None):
+    """Min-max scaler.
+
+    Args:
+        X: input data matrix (n_samples, n_features)
+        min_val: minimum value for scaling
+        max_val: maximum value for scaling
+
+    Returns:
+        X_scaled: scaled data matrix
+        min_val: minimum value for scaling
+        max_val: maximum value for scaling
+    """
+    if min_val is None:
+        min_val = X.min(axis=0)
+    if max_val is None:
+        max_val = X.max(axis=0)
+    X_scaled = (X - min_val) / (max_val - min_val)
+    return X_scaled, min_val, max_val
+
+
+def standard_scaler(X, mean=None, std=None):
+    """Standard scaler.
+
+    Args:
+        X: input data matrix (n_samples, n_features)
+        mean: mean value for scaling
+        std: standard deviation for scaling
+
+    Returns:
+        X_scaled: scaled data matrix
+        mean: mean value for scaling
+        std: standard deviation for scaling
+    """
+    if mean is None:
+        mean = X.mean(axis=0)
+    if std is None:
+        std = X.std(axis=0)
+    X_scaled = (X - mean) / std
+    return X_scaled, mean, std
+
+
 def onehot(y, num_classes):
     """
     Convert integer labels to one-hot encoding.
@@ -12,7 +54,9 @@ def onehot(y, num_classes):
     return y_onehot
 
 
-def make_linear_regression_dataset(n_samples=100, x_min=-10, x_max=10, w=None, noise=0.1, random_state=None):
+def make_linear_regression_dataset(
+    n_samples=100, x_min=-10, x_max=10, w=None, noise=0.1, random_state=None
+):
     x = np.random.uniform(x_min, x_max, n_samples)
     w = np.random.rand() if w is None else w
     b = np.random.rand()
@@ -20,7 +64,9 @@ def make_linear_regression_dataset(n_samples=100, x_min=-10, x_max=10, w=None, n
     return x, y
 
 
-def make_classification_dataset(n_samples=100, n_features=2, n_classes=2, cluster_std=1.5, random_state=None):
+def make_classification_dataset(
+    n_samples=100, n_features=2, n_classes=2, cluster_std=1.5, random_state=None
+):
     X, y = make_blobs(
         n_samples=n_samples,
         centers=n_classes,
@@ -46,23 +92,37 @@ def plot_loss_history(loss_history, iterations=None, interval=1):
     plt.ylabel("Loss")
 
 
-def plot_classification_dataset_2D(X, y, negative_label=-1, ax=None, colors=None, alpha=1, labels=None):
-    if colors is None:
-        colors = ["orange", "blue"]
-
+def plot_classification_dataset_2D(
+    X,
+    y,
+    negative_label=-1,
+    ax=None,
+    colors=["orange", "blue"],
+    alpha=1,
+    labels=["negative class", "positive class"],
+    loc="upper right",
+):
     if ax is None:
         ax = plt.gca()
 
-    if labels is None:
-        labels = ["negative class", "positive class"]
+    # Retrieve the existing legend handles and labels
+    handles_ax, labels_ax = ax.get_legend_handles_labels()
 
-    plt.scatter(
-        X[y == negative_label][:, 0], X[y == negative_label][:, 1], color=colors[0], edgecolors="gray", alpha=alpha
+    X_neg = X[y == negative_label]
+    X_pos = X[y == 1]
+    X = np.vstack([X_neg, X_pos])
+    scatter_neg = ax.scatter(
+        X_neg[:, 0], X_neg[:, 1], c=colors[0], edgecolors="gray", alpha=alpha, label=labels[0]
     )
-    ax.scatter(X[y == 1][:, 0], X[y == 1][:, 1], color=colors[1], edgecolors="gray", alpha=alpha)
+    scatter_pos = ax.scatter(
+        X_pos[:, 0], X_pos[:, 1], c=colors[1], edgecolors="gray", alpha=alpha, label=labels[1]
+    )
     ax.set_xlabel("$x_1$")
     ax.set_ylabel("$x_2$")
-    ax.legend(labels)
+
+    labels = labels_ax + labels
+    handles_ax += [scatter_neg, scatter_pos]
+    ax.legend(handles=handles_ax, labels=labels + labels_ax, loc=loc)
 
 
 def plot_decision_boundary_2D(X, w, ax=None, colors=None):
@@ -83,16 +143,11 @@ def plot_decision_boundary_2D(X, w, ax=None, colors=None):
     # Plot the decision boundary line
     ax.plot(x1, x2, color="black")
 
-    # plt.plot(x1, x2, color="black")
-
     # Fill the region above the decision boundary (shade in one color)
     ax.fill_between(x1, x2, max(x2.max(), X[:, 1].max()), color=colors[0], alpha=0.2)
 
-    # plt.fill_between(x1, x2, max(x2.max(), X[:, 1].max()), color=colors[0], alpha=0.2)
-
     # Fill the region below the decision boundary (shade in another color)
     ax.fill_between(x1, min(x2.min(), X[:, 1].min()), x2, color=colors[1], alpha=0.2)
-    # plt.fill_between(x1, min(x2.min(), X[:, 1].min()), x2, color=colors[1], alpha=0.2)
 
 
 def plot_xor(ax=None, colors=None):
